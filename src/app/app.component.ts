6f03e398a9d3fc4dc9f959f3b338321f14e0896b
irek02 <imirgaleev@gmail.com>
2019-10-08 18:53:34 -0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-stackoverflow';
+
+  number;
+  numberObs;
+
+  click() {
+
+    this.getRandomNumberPromise()
+      .then(randomNumber => {
+        this.number = randomNumber;
+      });
+
+    this.getRandomNumberObservable()
+      .subscribe(randomNumber => {
+        this.numberObs = randomNumber;
+      });
+
+  }
+
+  getRandomNumberPromise() {
+
+    return new Promise(resolve => {
+      resolve(Math.floor(Math.random() * 10));
+    });
+
+  }
+
+  getRandomNumberObservable() {
+
+    return new Observable(observer => {
+      observer.next(Math.floor(Math.random() * 10));
+    });
+
+  }
+
 }
