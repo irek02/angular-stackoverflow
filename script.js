26f79fcc3dd311cd70cfe698efb961a7d41eabd7
irek02 <imirgaleev@gmail.com>
2019-10-10 18:43:57 -0700

    init

---
+++ script.js
@@ -0,0 +1,25 @@
+const myPromise = new Promise(resolve => {
+  setTimeout(() => {
+    resolve('dog');
+  }, 100);
+});
+
+myPromise.then(result => {
+  console.log('promise: ', result);
+})
+
+const myObservable = new Rx.Observable(observer => {
+  setTimeout(() => {
+    observer.next('dog');
+    observer.next('cat');
+    observer.next('bird');
+  }, 100);
+});
+
+const subscription = myObservable
+  .filter(result => result === 'bird')
+  .subscribe(result => {
+    console.log('observable: ', result);
+  });
+
+subscription.unsubscribe();
